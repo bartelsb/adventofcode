cc2f2a7774a4089f84b8f91c89c7155639cc5ece
Ben Bartels <ben@phil.us>
2024-12-12 10:37:32 -0600

    Solve d2p1

--- 2024/2024.go
+++ 2024/2024.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/bartelsb/adventofcode/2024/day1"
+	"github.com/bartelsb/adventofcode/2024/day2"
 )
 
 func main() {
-	result := day1.Solve("./day1/input.txt")
+	result := day2.Solve("./day2/input.txt")
 	fmt.Println("Result is " + strconv.Itoa(result))
 }
---
+++ 2024/day2/day2.go
@@ -0,0 +1,62 @@
+package day2
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func Solve(inputFile string) int {
+	content, err := os.ReadFile(inputFile)
+	check(err)
+
+	reports := [][]int{}
+	for _, report := range strings.Split(string(content), "\n") {
+		reportArr := []int{}
+		for _, level := range strings.Split(report, " ") {
+			numLevel, err := strconv.Atoi(level)
+			check(err)
+			reportArr = append(reportArr, numLevel)
+		}
+		reports = append(reports, reportArr)
+	}
+
+	safeReports := 0
+	for _, report := range reports {
+		safe := true
+		direction := true
+		if report[0] != report[1] {
+			direction = checkDirection(report[0], report[1])
+		}
+		for i := 0; i < len(report)-1; i++ {
+			safe = checkLevelDiff(report[i], report[i+1], direction)
+			if !safe {
+				break
+			}
+		}
+		if safe {
+			safeReports++
+		}
+	}
+
+	return safeReports
+}
+
+func checkLevelDiff(level1, level2 int, direction bool) bool {
+	diff := level1 - level2
+	if direction {
+		return (diff >= -3) && (diff <= -1)
+	} else {
+		return (diff >= 1) && (diff <= 3)
+	}
+}
+
+func checkDirection(level1, level2 int) bool {
+	return level1 < level2
+}
